edfe23fac063bfe08816bd1fb1f0adb0a02047e8
Rohit Panwar <108470973+RohitPanwar03@users.noreply.github.com>
2025-05-15 17:50:17 +0530

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -5,6 +5,7 @@ export const generateToken = async (user, statusCode, res, message) => {
     httpOnly: true,
     expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
     secure: true,
+  sameSite: "None", 
   };
   res.status(statusCode).cookie("token", token, options).json({
     success: true,
