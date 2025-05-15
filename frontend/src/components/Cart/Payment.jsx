4df1060b43ef05a9a4a7ec746c912a48b730029a
Rohit Panwar <108470973+RohitPanwar03@users.noreply.github.com>
2025-05-15 20:14:55 +0530

    Update Payment.jsx

--- frontend/src/components/Cart/Payment.jsx
+++ frontend/src/components/Cart/Payment.jsx
@@ -58,7 +58,7 @@ const Payment = () => {
         },
       };
       const { data } = await axios.post(
-        "/api/v1/payment/process",
+        "https://ecommerce-7079.onrender.com/api/v1/payment/process",
         paymentData,
         config
       );
